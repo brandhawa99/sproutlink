b85348ec90ee973005f3c0544908a91a4a90328a
brandhawa99 <baltej.randhawa0@gmail.com>
2024-08-01 22:30:02 -0700

    update cors

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -8,6 +8,7 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		//if using cookies or auth tokens
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		if r.Method == "OPTIONS" {
