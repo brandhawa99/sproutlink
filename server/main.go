7d62f434429b07c5c27573ce03053e416183ae79
brandhawa99 <baltej.randhawa0@gmail.com>
2024-08-05 00:27:18 -0700

    updated main

--- server/main.go
+++ server/main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/brandhawa99/sproutlink/server/middleware"
+	"github.com/brandhawa99/sproutlink/server/routes"
 	"github.com/gofiber/fiber/v3/log"
 )
 
 func main() {
+	// created main server mux
 	mux := http.NewServeMux()
 
+	//middleware
 	stack := middleware.CreateStack(
 		middleware.Logging,
 		middleware.CORS,
 	)
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "hello")
-	}
-	mux.HandleFunc("/ping", h1)
+
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "pong")
+	})
+
+	// sub routers
+	userRouter := routes.UserRouter()
+	mux.Handle("/users/", http.StripPrefix("/users", userRouter))
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: stack(mux),
 	}
+
 	fmt.Println("Server Listening on Port 8080")
 	log.Fatal(server.ListenAndServe())
+
 }
