20f0cb05eb571eca65c1eb7e419be23b5fef8fa8
brandhawa99 <baltej.randhawa0@gmail.com>
2024-08-06 23:03:50 -0700

    created user model

---
+++ server/models/User.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Username string             `bson:"username,omitempty"`
+	Password []byte             `bson:"password,omitempty"`
+	Url      string             `bson:"url,omitempty"`
+}
